97b62102d5582018ccece4a00072a45bd844029f
Lautaro Galicchio <lau_g50@hotmail.com>
2023-04-01 23:28:45 -0300

    PreEntrega1Galicchio

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
-import navBar from './navBar'
+import NavBar from './Navbar/navBar'
 
 function App() {
   return (
     <div className='App'>
-    <navBar/>
+    <NavBar/>
     </div>
   );
 }
--- src/components/Navbar/navBar.js
+++ src/components/Navbar/navBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const navBar = () => {
+export const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
@@ -41,4 +41,4 @@ const navBar = () => {
   )
 }
 
-export default navBar
+export default NavBar
