f622643155e7954b043a8f24b8a156df4bc9c2df
Lautaro Galicchio <lau_g50@hotmail.com>
2023-05-29 11:37:35 -0300

    entregaFinalGalicchio

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -4,7 +4,12 @@ import { ItemList} from '../itemlist/itemlist';
 import { useParams } from 'react-router-dom';
 import { useDarkModeContext } from '../../context/DarkModeContext.js';
 import { getProducts } from "../../firebase/firebase.js"
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+
+
 export const ItemListContainer = () => {
+const imgs= require.context ('../../../public/img', true);
 const[productos,setproductos]=useState([])
 const { category}=useParams()
 
@@ -27,6 +32,8 @@ useEffect(() => {
         const productosFiltrados = productos.filter(prod => prod.stock > 0)
         setproductos(productosFiltrados)
 
+        
+
         })
     }
 
@@ -34,13 +41,43 @@ useEffect(() => {
 
     
     
-    return (
-        
-
+    return ( 
         <div className="row">
-            {(parseInt(category)===1)? <h3>Articulos</h3> : <h3></h3> }
-            {(parseInt(category)===2)? <h3>Pelotas</h3> : <h3></h3> }
-            {(parseInt(category)===3)? <h3>Maquinas</h3> : <h3></h3> }
+            {(parseInt(category)===1)? <h3>Articulos</h3> : <></> }
+            {(parseInt(category)===2)? <h3>Pelotas</h3> : <></> }
+            {(parseInt(category)===3)? <h3>Maquinas</h3> : <></> }
+            {(category)? <></> : 
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src={imgs('./entrenamientobarra.jpg')}class="d-block w-80" alt={"entrenamiento-barra"} />
+                    </div>
+                    <div class="carousel-item">
+                        <img src={imgs('./entrenamientoPadle.jpg')} class="d-block w-80" alt="entrenamiento-padle"></img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={imgs('./entrenamientoBasquet.jpg')} class="d-block w-80" alt="entrenamiento-basquet"></img>
+                    </div>
+        
+                    <div class="carousel-item">
+                        <img src={imgs('./entrenamientoBoxeo.jpg')} class="d-block w-80" alt="entrenamiento-boxeo"></img>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img src={imgs('./entrenamientoPesarusa.jpg')} class="d-block w-80" alt="entrenamiento-pesarusa"></img>
+                    </div>
+                </div>
+
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>       
+            </div>
+    }
     {<ItemList productos={productos} plantilla={"Item"} />}
     </div>
     
